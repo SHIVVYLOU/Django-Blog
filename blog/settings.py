0d393ac277504608b93af56f3af28574c8b58534
Ubuntu <ubuntu@ip-172-31-19-211.ec2.internal>
2019-09-03 12:30:23 +0000

    adjusted settings add whitenoise

--- blog/settings.py
+++ blog/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
